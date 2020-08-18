0c731d9f122a9c6c9158cf45ae1e2a25359fd35e
Amir <nik.amirsa@gmail.com>
2020-08-18 20:13:38 +0430

    Modularize codes

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+// Create volume btn
+let volumeBtn = document.createElement('span');
+volumeBtn.innerHTML =
+  '<button type="button" class="plyr__control" data-plyr="mute"><svg class="icon--pressed" aria-hidden="true" focusable="false"><use xlink:href="#plyr-muted"></use></svg><svg class="icon--not-pressed" aria-hidden="true" focusable="false"><use xlink:href="#plyr-volume"></use></svg></button>';
+
+document.querySelector('.plyr__controls__item.plyr__volume').style =
+  'display: none';
+
+// open and close volume control
+volumeBtn.addEventListener('mouseover', () => {
+  document.querySelector('.plyr__controls__item.plyr__volume').style =
+    'display: block';
+});
+
+volumeBtn.addEventListener('mouseout', () => {
+  document.querySelector('.plyr__controls__item.plyr__volume').style =
+    'display: none';
+});
+
+document
+  .querySelector('.plyr__controls__item.plyr__volume')
+  .addEventListener('mouseover', () => {
+    document.querySelector('.plyr__controls__item.plyr__volume').style =
+      'display: block';
+  });
+
+document
+  .querySelector('.plyr__controls__item.plyr__volume')
+  .addEventListener('mouseout', () => {
+    document.querySelector('.plyr__controls__item.plyr__volume').style =
+      'display: none';
+  });
+
+document.querySelector('input[data-plyr=volume]').style =
+  'background: rgb(32, 45, 58); padding: 15px !important; border-radius: 5px; width: 150px;';
+
+document.querySelector('.plyr__controls__item.plyr__control').after(volumeBtn);
---
+++ assets/js/plyr-config.js
@@ -0,0 +1,41 @@
+const player = new Plyr('#player', {
+  controls: [
+    'play-large',
+    'play',
+    'volume',
+    'current-time',
+    'progress',
+    'duration',
+    'rewind',
+    'fast-forward',
+    'captions',
+    'settings',
+    'fullscreen',
+  ],
+  i18n: {
+    play: 'پخش',
+    pause: 'توقف',
+    enterFullscreen: 'تمام صفحه',
+    exitFullscreen: 'خروج از تمام صفحه',
+    settings: 'تنظیمات',
+    speed: 'سرعت پخش',
+    normal: 'عادی',
+    enableCaptions: 'فعال‌کردن زیرنویس',
+    disableCaptions: 'غیرفعال‌کردن زیرنویس',
+    rewind: '10 ثانیه به عقب‌',
+    fastForward: '10 ثانیه به جلو',
+  },
+
+  seekTime: 10,
+  hideControls: false,
+  settings: ['speed'],
+  tooltips: {
+    controls: true,
+    seek: true,
+  },
+});
+
+(() => {
+  const list = document.querySelector('.plyr');
+  list.appendChild(list.firstElementChild);
+})();

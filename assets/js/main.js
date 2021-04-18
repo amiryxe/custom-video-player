deb2d62522917a4336a09d5c6a677e57d3d5bc82
Amir <nik.amirsa@gmail.com>
2021-04-18 11:10:29 +0430

    fix some problem in multiple mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,63 @@
 // Create volume btn
-let volumeBtn = document.createElement('span');
+let volumeBtn = document.createElement("span");
 volumeBtn.innerHTML =
   '<button type="button" class="plyr__control" data-plyr="mute"><svg class="icon--pressed" aria-hidden="true" focusable="false"><use xlink:href="#plyr-muted"></use></svg><svg class="icon--not-pressed" aria-hidden="true" focusable="false"><use xlink:href="#plyr-volume"></use></svg></button>';
 
-document.querySelector('.plyr__controls__item.plyr__volume').style =
-  'display: none';
+document
+  .querySelectorAll(".plyr__controls__item.plyr__volume")
+  .forEach((item) => {
+    item.style = "display: none";
+  });
 
 // open and close volume control
-volumeBtn.addEventListener('mouseover', () => {
-  document.querySelector('.plyr__controls__item.plyr__volume').style =
-    'display: block';
+volumeBtn.addEventListener("mouseover", () => {
+  document
+    .querySelectorAll(".plyr__controls__item.plyr__volume")
+    .forEach((item) => {
+      item.style = "display: block";
+    });
 });
 
-volumeBtn.addEventListener('mouseout', () => {
-  document.querySelector('.plyr__controls__item.plyr__volume').style =
-    'display: none';
+volumeBtn.addEventListener("mouseout", () => {
+  document
+    .querySelectorAll(".plyr__controls__item.plyr__volume")
+    .forEach((item) => {
+      item.style = "display: none";
+    });
 });
 
 document
-  .querySelector('.plyr__controls__item.plyr__volume')
-  .addEventListener('mouseover', () => {
-    document.querySelector('.plyr__controls__item.plyr__volume').style =
-      'display: block';
+  .querySelectorAll(".plyr__controls__item.plyr__volume")
+  .forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      document
+        .querySelectorAll(".plyr__controls__item.plyr__volume")
+        .forEach((item) => {
+          item.style = "display: none";
+        });
+    });
   });
 
 document
-  .querySelector('.plyr__controls__item.plyr__volume')
-  .addEventListener('mouseout', () => {
-    document.querySelector('.plyr__controls__item.plyr__volume').style =
-      'display: none';
+  .querySelectorAll(".plyr__controls__item.plyr__volume")
+  .forEach((item) => {
+    item.addEventListener("mouseout", () => {
+      document
+        .querySelectorAll(".plyr__controls__item.plyr__volume")
+        .forEach((item) => {
+          item.style = "display: none";
+        });
+    });
   });
 
 // force styles
-document.querySelector('input[data-plyr=volume]').style =
-  'background: rgb(32, 45, 58); padding: 15px !important; border-radius: 5px; width: 150px;';
+document.querySelectorAll("input[data-plyr=volume]").style =
+  "background: rgb(32, 45, 58); padding: 15px !important; border-radius: 5px; width: 150px;";
 
 document
-  .querySelector('input[data-plyr=volume]')
-  .classList.add('plyr__tooltip-volume');
+  .querySelectorAll("input[data-plyr=volume]")
+  .forEach((item) => item.classList.add("plyr__tooltip-volume"));
 
-document.querySelector('.plyr__controls__item.plyr__control').after(volumeBtn);
+document
+  .querySelectorAll(".plyr__controls__item.plyr__control")
+  .forEach((item) => item.after(volumeBtn));
--- assets/js/plyr-config.js
+++ assets/js/plyr-config.js
@@ -1,34 +1,34 @@
-const player = new Plyr('#player', {
+const player = Plyr.setup(".player", {
   controls: [
-    'play-large',
-    'play',
-    'volume',
-    'current-time',
-    'progress',
-    'duration',
-    'rewind',
-    'fast-forward',
-    'captions',
-    'settings',
-    'fullscreen',
+    "play-large",
+    "play",
+    "volume",
+    "current-time",
+    "progress",
+    "duration",
+    "rewind",
+    "fast-forward",
+    "captions",
+    "settings",
+    "fullscreen",
   ],
   i18n: {
-    play: 'پخش',
-    pause: 'توقف',
-    enterFullscreen: 'تمام صفحه',
-    exitFullscreen: 'خروج از تمام صفحه',
-    settings: 'سرعت پخش',
-    speed: 'انتخاب سرعت',
-    normal: 'عادی',
-    enableCaptions: 'فعال‌کردن زیرنویس',
-    disableCaptions: 'غیرفعال‌کردن زیرنویس',
-    rewind: '10 ثانیه به عقب‌',
-    fastForward: '10 ثانیه به جلو',
+    play: "پخش",
+    pause: "توقف",
+    enterFullscreen: "تمام صفحه",
+    exitFullscreen: "خروج از تمام صفحه",
+    settings: "سرعت پخش",
+    speed: "انتخاب سرعت",
+    normal: "عادی",
+    enableCaptions: "فعال‌کردن زیرنویس",
+    disableCaptions: "غیرفعال‌کردن زیرنویس",
+    rewind: "10 ثانیه به عقب‌",
+    fastForward: "10 ثانیه به جلو",
   },
 
   seekTime: 10,
   hideControls: false,
-  settings: ['speed'],
+  settings: ["speed"],
   tooltips: {
     controls: true,
     seek: true,
@@ -36,6 +36,6 @@ const player = new Plyr('#player', {
 });
 
 (() => {
-  const list = document.querySelector('.plyr');
+  const list = document.querySelector(".plyr");
   list.appendChild(list.firstElementChild);
 })();

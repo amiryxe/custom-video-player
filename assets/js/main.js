49a5ea218c0fcbe5c8f1a1bdb514ccd26efcc88d
Amir <nik.amirsa@gmail.com>
2021-04-18 13:22:02 +0430

    show volume btn after play button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,45 @@
 // Create volume btn
-let volumeBtn = document.createElement("span");
-volumeBtn.innerHTML =
-  '<button type="button" class="plyr__control" data-plyr="mute"><svg class="icon--pressed" aria-hidden="true" focusable="false"><use xlink:href="#plyr-muted"></use></svg><svg class="icon--not-pressed" aria-hidden="true" focusable="false"><use xlink:href="#plyr-volume"></use></svg></button>';
+
+// force styles
+document.querySelectorAll("input[data-plyr=volume]").style =
+  "background: rgb(32, 45, 58); padding: 15px !important; border-radius: 5px; width: 150px;";
 
 document
-  .querySelectorAll(".plyr__controls__item.plyr__volume")
+  .querySelectorAll("input[data-plyr=volume]")
+  .forEach((item) => item.classList.add("plyr__tooltip-volume"));
+
+document
+  .querySelectorAll(".plyr__controls__item.plyr__control")
   .forEach((item) => {
-    item.style = "display: none";
+    let volumeBtn = document.createElement("span");
+    volumeBtn.innerHTML =
+      '<button type="button" class="plyr__control" data-plyr="mute"><svg class="icon--pressed" aria-hidden="true" focusable="false"><use xlink:href="#plyr-muted"></use></svg><svg class="icon--not-pressed" aria-hidden="true" focusable="false"><use xlink:href="#plyr-volume"></use></svg></button>';
+
+    document
+      .querySelectorAll(".plyr__controls__item.plyr__volume")
+      .forEach((item) => {
+        item.style = "display: none";
+      });
+
+    // open and close volume control
+    volumeBtn.addEventListener("mouseover", () => {
+      document
+        .querySelectorAll(".plyr__controls__item.plyr__volume")
+        .forEach((item) => {
+          item.style = "display: block";
+        });
+    });
+
+    volumeBtn.addEventListener("mouseout", () => {
+      document
+        .querySelectorAll(".plyr__controls__item.plyr__volume")
+        .forEach((item) => {
+          item.style = "display: none";
+        });
+    });
+
+    item.after(volumeBtn);
   });
-
-// open and close volume control
-volumeBtn.addEventListener("mouseover", () => {
-  document
-    .querySelectorAll(".plyr__controls__item.plyr__volume")
-    .forEach((item) => {
-      item.style = "display: block";
-    });
-});
-
-volumeBtn.addEventListener("mouseout", () => {
-  document
-    .querySelectorAll(".plyr__controls__item.plyr__volume")
-    .forEach((item) => {
-      item.style = "display: none";
-    });
-});
 
 document
   .querySelectorAll(".plyr__controls__item.plyr__volume")
@@ -49,15 +64,3 @@ document
         });
     });
   });
-
-// force styles
-document.querySelectorAll("input[data-plyr=volume]").style =
-  "background: rgb(32, 45, 58); padding: 15px !important; border-radius: 5px; width: 150px;";
-
-document
-  .querySelectorAll("input[data-plyr=volume]")
-  .forEach((item) => item.classList.add("plyr__tooltip-volume"));
-
-document
-  .querySelectorAll(".plyr__controls__item.plyr__control")
-  .forEach((item) => item.after(volumeBtn));
